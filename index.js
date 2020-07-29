9eabe22307bb0a7ec3e79747ee6bd1a92984d49d
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-07-29 22:09:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+const text = document.querySelector('#text');
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    text.innerHTML = "This is really cool!";
+  });
+
+   
